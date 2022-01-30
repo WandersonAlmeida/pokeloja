ef2de718676614e6d0268bca5e9ab1fbc43f9ff1
wanderson almeida <https://github.com/WandersonAlmeida/tabuada>
2022-01-30 20:26:20 -0300

    refatorando carrinho remove

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,87 +1,104 @@
-
 function openCart() {
-    const btncar = document.querySelector('#car');
-    btncar.addEventListener('click', function (event) {
-        event.preventDefault();
-        document.body.className = document.body.className === ''? "carrinho-aberto" : "";
-        renderCart();
-    });
+  const btncar = document.querySelector("#car");
+  btncar.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.body.className =
+      document.body.className === "" ? "carrinho-aberto" : "";
+    renderCart();
+  });
 }
-
-
 
 function closeCart() {
-    const fecharCar = document.querySelector('#fecharCar')
-    const btnfecharCarrinho = document.querySelector('#fecharCarrinho');
+  const fecharCar = document.querySelector("#fecharCar");
+  const btnfecharCarrinho = document.querySelector("#fecharCarrinho");
 
-    fecharCar.addEventListener('click', function (event) {
-        event.preventDefault();
-        document.body.className = '';
-    })
+  fecharCar.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.body.className = "";
+  });
 
-    document.addEventListener('keyup', (event) => {
-        const keyName = event.key;
-        if (keyName === 'Escape') {
-            document.body.className = "";
-        }
-    });
+  document.addEventListener("keyup", (event) => {
+    const keyName = event.key;
+    if (keyName === "Escape") {
+      document.body.className = "";
+    }
+  });
 
-    btnfecharCarrinho.addEventListener('click', function (event) {
-        event.preventDefault();
-        if (document.body.className === "carrinho-aberto") {
-            document.body.className = "";
-        }
-        
-    });
+  btnfecharCarrinho.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (document.body.className === "carrinho-aberto") {
+      document.body.className = "";
+    }
+  });
 }
 
-function renderCart(){
-
-    const pokemonCartList = document.querySelector("#pokemonsAdded");
-    let html ="<ul>"; 
-    const pokemonsAdded = getLocalStorage()
-    pokemonsAdded.forEach((pokemon)=>{
-        html+=`<li><img class = "imageCar" src= "${pokemon?.image}"alt= "${pokemon?.name}"/></li>
-        <li class = "nameCar"> ${pokemon?.name}</li>
-        <li class = "priceCar">R$ ${(pokemon?.price * 0.8).toFixed(2)}</li>
-        <div class ="quantidade"> <button class="removeItem" onclick="remove('${pokemon.id}')" >
+function renderCart() {
+  const pokemonCartList = document.querySelector("#pokemonsAdded");
+  let html = "<ul>";
+  const pokemonsAdded = getLocalStorage();
+  pokemonsAdded.forEach((pokemonObj) => {
+    html += `<li><img class = "imageCar" src= "${
+      pokemonObj.pokemon.image
+    }"alt= "${pokemonObj.pokemon.name}"/></li>
+        <li class = "nameCar"> ${pokemonObj.pokemon.name}</li>
+        <li class = "priceCar">R$ ${(
+          pokemonObj.pokemon.price * pokemonObj.quantity
+        ).toFixed(2)}</li>
+        <li class = "quantity"> Quantidade = ${pokemonObj.quantity}</li>
+        <div class ="quantidade"> <button class="removeItem" onclick="remove('${
+          pokemonObj.pokemon.id
+        }')" >
             Excluir
         </button>
         </div>
         `;
-        
-    });
+  });
 
-    html+=`</ul>`;
-    pokemonCartList.innerHTML = html; 
-}
-function remove(pokemonid){
-    const removeItem = document.querySelector(".removeItem");
-    const pokemonsAdded = getLocalStorage();
-    const local = pokemonsAdded.filter(pokemon=>pokemon.id!== pokemonid)
-        
-      
-    
-    localStorage.setItem("pokemonsCart",JSON.stringify(local));
-} 
-    
-function getLocalStorage(){
-    return JSON.parse(localStorage.getItem("pokemonsCart")) || [];
-}
-function addPokemon(pokemon){
-    const pokemonsAdded = getLocalStorage();
-    pokemonsAdded.push(pokemon)
-    
-    
-
-    
-    localStorage.setItem("pokemonsCart",JSON.stringify(pokemonsAdded));
+  html += `</ul>`;
+  pokemonCartList.innerHTML = html;
 }
 
+function remove(pokemonid) {
+  //const removeItem = document.querySelector(".removeItem");
+  const pokemonsAdded = getLocalStorage();
+  const local = pokemonsAdded.filter((pokemonObj) => {
+    if (pokemonObj.pokemon.id !== pokemonid) {
+      return true;
+    }
+    pokemonObj.quantity = pokemonObj.quantity - 1;
+    if (pokemonObj.quantity == 0) {
+      return false;
+    }
+    return true;
+  });
 
-window.addEventListener("load",async()=>{
-    console.log("load carrinho.js");  
-    openCart();
-    closeCart();
+  localStorage.setItem("pokemonsCart", JSON.stringify(local));
+  renderCart();
+}
+
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem("pokemonsCart")) || [];
+}
+
+function addPokemon(pokemon) {
+  const pokemonsAdded = getLocalStorage();
+  let quantity = 0;
+  const newPokemonsAdded = pokemonsAdded.filter((pokemonObj) => {
+    if (pokemonObj.pokemon.id === pokemon.id) {
+      quantity = pokemonObj.quantity;
+      return false;
+    }
+    return true;
+  });
+
+  newPokemonsAdded.push({ pokemon, quantity: quantity + 1 });
+
+  localStorage.setItem("pokemonsCart", JSON.stringify(newPokemonsAdded));
+}
+
+window.addEventListener("load", async () => {
+  console.log("load carrinho.js");
+  openCart();
+  closeCart();
 });
 //const local = pokemonsAdded.filter(pokemon=>pokemon.id!== pokemonid)
--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 class Pokemon {
-    constructor(name, url,) {
-        this.name = name;
-        this.url = url;          //https://pokeapi.co/api/v2/pokemon/1/
-        this.id = this.url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', '');
-        this.image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${this.id}.png`;
-        this.price = Math.floor(Math.random() * 200);
-    }
-    html() {
-        const pokediv = document.createElement('div');
-        pokediv.className = 'product';
-        pokediv.innerHTML = `  
+  constructor(name, url) {
+    this.name = name;
+    this.url = url; //https://pokeapi.co/api/v2/pokemon/1/
+    this.id = this.url
+      .replace("https://pokeapi.co/api/v2/pokemon/", "")
+      .replace("/", "");
+    this.image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${this.id}.png`;
+    this.price = Math.floor(Math.random() * 200);
+  }
+
+  html() {
+    const pokediv = document.createElement("div");
+    pokediv.className = "product";
+    pokediv.innerHTML = `  
             <a href="pokemon.html?id=${this.id}">
             <img class="card-img" src="${this.image}" alt='${this.name}'/></a>
             <h2 class="nome">${this.name}</h2>
@@ -18,105 +21,109 @@ class Pokemon {
             <button class="btn btn-add-pokemon " data-id="${this.id}" >
                 <img  src="image/pokebola.png" alt='pokebola'/> comprar
             </button>`;
-        return pokediv;
-    }
+    return pokediv;
+  }
 }
 
-function render (pokemonsList){
-    const listaPokemon = document.querySelector(".listaPokemon");
-    listaPokemon.innerHTML = "";
-    const pokemonsObjectList = pokemonsList.map((pokemon) => new Pokemon(pokemon.name,pokemon.url));
+function render(pokemonsList) {
+  const listaPokemon = document.querySelector(".listaPokemon");
+  listaPokemon.innerHTML = "";
+  const pokemonsObjectList = pokemonsList.map(
+    (pokemon) => new Pokemon(pokemon.name, pokemon.url)
+  );
 
-    pokemonsObjectList.forEach((pokemon)=>{
-        const html = pokemon.html();
-        listaPokemon.appendChild(html);
+  pokemonsObjectList.forEach((pokemon) => {
+    const html = pokemon.html();
+    listaPokemon.appendChild(html);
+  });
+  const btnAddPokemon = document.querySelectorAll(".btn-add-pokemon");
+  btnAddPokemon.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      const id = event.target.getAttribute("data-id");
+      const pokemon = pokemonsObjectList.find(
+        (pokemonObject) => pokemonObject.id == id
+      );
+      addPokemon(pokemon);
     });
-    const btnAddPokemon = document.querySelectorAll(".btn-add-pokemon");
-    btnAddPokemon.forEach((btn)=>{
-        btn.addEventListener("click",(event)=>{
-            const id = event.target.getAttribute("data-id");
-            const pokemon = pokemonsObjectList.find((pokemonObject)=>pokemonObject.id == id);
-            addPokemon(pokemon);
-        });
-    });
-};
+  });
+}
 
 async function getPokemons(page = 0) {
-    const listaPokemon = document.querySelector('.listaPokemon');
-    listaPokemon.innerHTML ='<div class = "carregando">Carregando pokemons...</dic>';
-    await fakePromise();
-    const limit = 20;
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset= ${limit * page}`);
-    const json = await response.json();
-    const pages = Math.ceil(json.count / limit);
-    return {result: json.results,totalPage:pages};
+  const listaPokemon = document.querySelector(".listaPokemon");
+  listaPokemon.innerHTML =
+    '<div class = "carregando">Carregando pokemons...</dic>';
+  await fakePromise();
+  const limit = 20;
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset= ${limit * page}`
+  );
+  const json = await response.json();
+  const pages = Math.ceil(json.count / limit);
+  return { result: json.results, totalPage: pages };
 }
 
-const fakePromise = ()=>new Promise((resolve)=>setTimeout(resolve,2000));
+const fakePromise = () => new Promise((resolve) => setTimeout(resolve, 2000));
 
 function hiddenPrevious(page) {
-    const btnAnt= document.querySelector('.btn-ant');
-    btnAnt.style.visibility = page === 0 ? 'hidden': 'visible';
-    return btnAnt;
+  const btnAnt = document.querySelector(".btn-ant");
+  btnAnt.style.visibility = page === 0 ? "hidden" : "visible";
+  return btnAnt;
 }
 //function para verificar se tem pag e fazer sumir o botao
-function hiddenNext(page,totalPage) { 
-    const btnProx = document.querySelector('.btn-prox');
-    btnProx.style.visibility = page == totalPage ? 'hidden' :"visible ";
-    return btnProx;
+function hiddenNext(page, totalPage) {
+  const btnProx = document.querySelector(".btn-prox");
+  btnProx.style.visibility = page == totalPage ? "hidden" : "visible ";
+  return btnProx;
 }
-
 
 //function para voltar a pag
 function previousPage(page) {
-    const btnAnt = document.querySelector('.btn-ant');
-    btnAnt.onclick = async () => {
-        updateQueryParametes(page-1);
-    };
-};
+  const btnAnt = document.querySelector(".btn-ant");
+  btnAnt.onclick = async () => {
+    updateQueryParametes(page - 1);
+  };
+}
 //function para passar para a proxima pag
 function nextPage(page) {
-    const btnProx = document.querySelector('.btn-prox');
-        btnProx.onclick = async () => {
-            updateQueryParametes(page+1);
-        };
+  const btnProx = document.querySelector(".btn-prox");
+  btnProx.onclick = async () => {
+    updateQueryParametes(page + 1);
+  };
 }
 
-function updatePageNumber(page){
-    const pag = document.querySelector(".pag")
-    pag.innerHTML = page != 0 ?`${page}`:"";
+function updatePageNumber(page) {
+  const pag = document.querySelector(".pag");
+  pag.innerHTML = page != 0 ? `${page}` : "";
 }
-function getPageNumber(){
-    const params = getQueryParamates();
-    return parseInt(params.page || 0);
+function getPageNumber() {
+  const params = getQueryParamates();
+  return parseInt(params.page || 0);
 }
-function getQueryParamates(page){
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());
-    return params;
+function getQueryParamates(page) {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+  return params;
 }
-function updateQueryParametes(page){
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    urlSearchParams.set("page",page);
-    window.location.search = urlSearchParams.toString();
+function updateQueryParametes(page) {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  urlSearchParams.set("page", page);
+  window.location.search = urlSearchParams.toString();
 }
-
 
 //executa com a pag termina de carregar
 window.onload = async () => {
-    const page = getPageNumber();
+  const page = getPageNumber();
 
-    updatePageNumber(page);
+  updatePageNumber(page);
 
-    const response = await getPokemons(page);
-    render(response.result);
-    
-    hiddenPrevious(page);
+  const response = await getPokemons(page);
+  render(response.result);
 
-    hiddenNext(page,response.totalPage);
+  hiddenPrevious(page);
 
-    previousPage(page);
+  hiddenNext(page, response.totalPage);
 
-    nextPage(page);
+  previousPage(page);
 
-}
+  nextPage(page);
+};
